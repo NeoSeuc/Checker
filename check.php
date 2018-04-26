800acdee80fd88e8a71a3b8e3afd0be78fc272f9
NeoSeuc <vitaliy199718@gmail.com>
2018-04-26 21:05:22 +0300

    fix messaging

--- check.php
+++ check.php
@@ -11,20 +11,28 @@ $request_param = [
     'text' => $msg
 ];
 
-$count = count($users);
+
 
 
 $db = Db::getConnection();
+for($i = 0; $i<count($users); $i++) {
+    $db->query("INSERT INTO chat_id (id) VALUES ('{$users[$i]}')");
+}
+$chat_id_result = $db->query("SELECT * FROM chat_id");
+while( $row1 = $chat_id_result->fetch()){
+    $chat_id[] = $row1['id']; // Inside while loop
+}
 
 $result = $db->query('SELECT * from sites');
+$count = count($chat_id);
 require_once('Testing.php');
 while ($row = $result->fetch()) {
     $res = Testing::testSite($row['name']);
     if (!$res) {
-        for ($i = 0; $i < 3; $i++) {
+        for ($i = 0; $i < $count; $i++) {
             $request_param['text'] = $row['name'] . $msg;
 
-            $request_param['chat_id'] = $users[$i];
+            $request_param['chat_id'] = $chat_id[$i];
 
             $request_url = "https://api.telegram.org/bot" . $token . "/sendMessage?" . http_build_query($request_param);
 
